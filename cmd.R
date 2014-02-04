99cc12a0c6b52bce62259d80f66c042b46fb1f60
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2014-02-04 18:38:47 +0900

    Create cmd.R

---
+++ cmd.R
@@ -0,0 +1,25 @@
+
+#mv
+mv(input, output){
+	cmdline <- paste(
+	  "mv", input, output,
+	  sep=" ")
+	system(cmdline)
+}
+
+#cp
+cp(input, output){
+	cmdline <- paste(
+	  "cp", input, output,
+	  sep=" ")
+	system(cmdline)
+}
+
+#sox example
+sox(input, output){ 
+	cmdline <- paste(
+	  "sox ", input, " -r 8000 -c 1 ", output, " trim 0 20",
+	  sep="")
+	system(cmdline)
+}
+
